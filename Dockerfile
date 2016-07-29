904fd6b24512b47aeace51b92877d1e15d35280b
Wurstmeister <wurstmeister@users.noreply.github.com>
2016-07-29 16:12:17 +0100

    updated mesos plugin
    * support dependency resolution for plugins

--- Dockerfile
+++ Dockerfile
@@ -37,7 +37,7 @@ RUN chmod +x ./init.sh                                   \
  && chmod +x /opt/scripts/marathon_env_init.sh           \
  && cp -R /usr/share/jenkins/ref/* /var/lib/jenkins      \
  && sleep 1                                              \
- && /opt/scripts/fetch-jenkins-plugins.sh $PLUGIN_DEFS   \
+ && /opt/scripts/fetch-jenkins-plugins.sh $PLUGIN_DEFS /tmp/plugins  \
  && chown -R jenkins:jenkins /usr/share/jenkins          \
  && chown -R jenkins:jenkins /var/lib/jenkins            \
  && chown -R jenkins:jenkins /var/log/jenkins
--- skel/opt/scripts/fetch-jenkins-plugins.sh
+++ skel/opt/scripts/fetch-jenkins-plugins.sh
@@ -1,23 +1,76 @@
 #!/bin/bash
 
-if [[ ! -f $1 ]]; then
-  echo "No plugin list specified."
-  exit
+UPDATES_URL="http://updates.jenkins-ci.org/download/plugins/"
+
+if [ $# -lt 2 ]; then
+  echo "USAGE: $0 plugin-list-file destination-directory"
+  exit 1
 fi
 
-plugin_dir=$JENKINS_HOME/plugins
-mkdir -p "$plugin_dir"
+plugin_list=$1
+plugin_dir=$2
 
-jenkins_dl=https://updates.jenkins-ci.org/download/plugins
+#file_owner=jenkins.jenkins
 
-while read -r plugin_raw
-do
-if echo "$plugin_raw" | grep -E -q '[^#|\s*$]' ; then
-  plugin=(${plugin_raw//:/ })
-  if [[ -z ${plugin[1]} ]]; then
-    plugin[1]="latest"
+mkdir -p $plugin_dir
+
+installPlugin() {
+  if [ -f ${plugin_dir}/${1}.hpi -o -f ${plugin_dir}/${1}.jpi ]; then
+    if [ "$2" == "1" ]; then
+      return 1
+    fi
+    echo "Skipped: $1 (already installed)"
+    return 99
+  else
+    echo "Installing: $1 ${UPDATES_URL}/${1}/${2}/${1}.hpi"
+    curl -L --silent --output ${plugin_dir}/${1}.hpi  ${UPDATES_URL}/${1}/${2}/${1}.hpi
+    return 0
   fi
-  echo "Fetching plugin: ${plugin[0]}:${plugin[1]}"
-  wget -P "$plugin_dir" "$jenkins_dl/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi"
-fi
-done < "$1"
+}
+
+while IFS=":" read plugin version
+do
+    #escape comments
+    if [[ $plugin =~ ^# ]]; then
+       continue
+    fi
+
+    #install the plugin
+    installPlugin $plugin $version
+done < $plugin_list
+
+
+changed=1
+maxloops=100
+
+while [ "$changed"  == "1" ]; do
+  echo "Check for missing dependecies ..."
+  if  [ $maxloops -lt 1 ] ; then
+    echo "Max loop count reached - probably a bug in this script: $0"
+    exit 1
+  fi
+  ((maxloops--))
+  changed=0
+  for f in ${plugin_dir}/*.hpi ; do
+    echo "checking $f"
+    # get a list of only non-optional dependencies
+    deps=$( unzip -p ${f} META-INF/MANIFEST.MF|tr -d '\r' | sed -e ':a;N;$!ba;s/\n //g' | grep -e 'Plugin-Dependencies' | awk '{print $2}' | tr "," "\n" | grep -v 'resolution:=optional')
+    echo "Deps=$deps"
+    #if deps were found, install them .. then set changed, so we re-loop all over all xpi's 
+    if [[ ! -z $deps ]]; then
+       echo $deps | tr ' ' '\n' | 
+       while IFS=: read plugin version; do
+          installPlugin $plugin $version
+          rc=$?; if [[ $rc == 0 ]]; then changed=1; fi
+       done
+       #changed=1
+    fi
+  done
+done
+
+#echo "fixing permissions"
+
+#chown ${file_owner} ${plugin_dir} -R
+
+echo "all done"
+
